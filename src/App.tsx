c9ff842febd56d00a961ce3559d1f2cf1f847faa
Gal Erzen Pajic <gal.erzen.pajic@gmail.com>
2021-05-02 17:49:42 +0200

    Feat: add routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Login from "./components/Login/index";
+import {Route, Switch, Redirect} from "react-router-dom";
+import NotFound from "./components/NotFound";
 
 function App() {
-  return <div className="App">App</div>;
+  return( 
+  <>
+  <Switch>
+    <Route exact path="/" component={Login} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+  </>);
 }
 
 export default App;
