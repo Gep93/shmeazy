1fc2e38e5472a41cfcd922aeaf1146557068da2a
Gal Erzen Pajic <gal.erzen.pajic@gmail.com>
2021-05-10 18:29:37 +0200

    Feat: redirect user to fix-cookies path when localstorage not available due to cookie permissions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.css";
+import {useEffect} from "react";
 import Login from "./components/Login/index";
-import {Route, Switch, Redirect} from "react-router-dom";
+import {Route, Switch, Redirect, useHistory} from "react-router-dom";
 import NotFound from "./components/NotFound";
+import FixCookies from "./components/FixCookies/index";
+import localStorageAvailable from "./helpers/localstorage";
 
 function App() {
+  const history = useHistory();
+  useEffect(() => {
+    if(!localStorageAvailable()) history.push("/fix-cookies");
+});
+
   return( 
   <>
   <Switch>
     <Route exact path="/" component={Login} />
     <Route exact path="/not-found" component={NotFound} />
+    <Route exact path="/fix-cookies" component={FixCookies} />
     <Redirect to="/not-found" />
   </Switch>
   </>);
---
+++ src/components/FixCookies/index.tsx
@@ -0,0 +1,7 @@
+const FixCookies = () => {
+    return(
+        <h1 style={{color: "white"}}>Please allow this website to use cookies in your browsers security settings.</h1>
+    );
+}
+
+export default FixCookies;
